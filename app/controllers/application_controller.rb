d98da6f0f50df2ade9c8bb28b26e6b89f2d11cbe
ipc103 <iancandysemail@gmail.com>
2017-01-03 13:44:59 -0500

    comment out our form for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  def form_for(obj)
-    unless obj.persisted?
-      method = 'POST'
-      action = "/#{obj.class.downcase.pluralize}"
-    else
-      method = 'PATCH'
-      action = "/#{obj.class.downcase.pluralize}/#{obj.id}"
-    end
-    "<form method='#{method}' action='#{action}'>"
-  end
+  # def form_for(obj)
+  #   unless obj.persisted?
+  #     method = 'POST'
+  #     action = "/#{obj.class.downcase.pluralize}"
+  #   else
+  #     method = 'PATCH'
+  #     action = "/#{obj.class.downcase.pluralize}/#{obj.id}"
+  #   end
+  #   "<form method='#{method}' action='#{action}'>"
+  # end
 end
