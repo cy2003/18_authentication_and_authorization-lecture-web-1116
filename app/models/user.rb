926dbe7121bc6c5d00a4950c11900e0bf1b85042
ipc103 <iancandysemail@gmail.com>
2017-01-04 09:42:34 -0500

    starter code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :tweets
-  has_secure_password
   validates :username, presence: true, uniqueness: true, exclusion: {in: %w(ryanseacrest carsondaly)}
 
 end
